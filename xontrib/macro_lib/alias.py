9ffff3f09f326d5b8506e034dfe0d4727fe11977
Andy Kipp <anki-code@users.noreply.github.com>
2022-12-03 14:51:38 +0600

    Update alias.py

--- xontrib/macro_lib/alias.py
+++ xontrib/macro_lib/alias.py
@@ -14,7 +14,7 @@ class Alias(Block):
     def __exit__(self, *a, **kw):
         fn_str = 'def _a(args):\n' + indent(self.macro_block, '    ')
         code_obj = XSH.builtins.compilex(fn_str, filename=f'<xonsh-alias-{self.alias_name}>', mode='exec', glbs=XSH.ctx)
-        fn = types.FunctionType(code_obj.co_consts[0], globals())
+        fn = types.FunctionType(code_obj.co_consts[0], globals())  # https://stackoverflow.com/a/57804755
         aliases[self.alias_name] = fn
 
 # with! Alias('e1'):
