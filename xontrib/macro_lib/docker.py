eac483fe05afb007019c583c38ee4f556734b5dc
Andy Kipp <anki-code@users.noreply.github.com>
2023-03-17 21:01:01 +0600

    Update docker.py

--- xontrib/macro_lib/docker.py
+++ xontrib/macro_lib/docker.py
@@ -1,13 +1,28 @@
 from xonsh.contexts import Block
 
-class RunInXonshDocker(Block):
-    """Run xonsh codeblock in the xonsh docker container."""
-
-    def __init__(self, image='xonsh/xonsh:slim'):
-       self.docker_image = image
+class RunInDocker(Block):
+    """Run codeblock in docker container."""
+    
+    def __init__(self, image='ubuntu', executor='bash'):
+        """
+        Parameters
+        ----------
+            image : str
+                The docker image name. Default: ubuntu
+            executor : str
+                The path to executor. Default: bash 
+        """        
+        self.docker_image = image
+        self.docker_executor = executor
 
     def __exit__(self, *a, **kw):
-        cmd = f"docker run -it --rm {self.docker_image} /usr/local/bin/xonsh -c {repr(self.macro_block)}"
-        __xonsh__.builtins.execx(cmd, 'exec', __xonsh__.ctx, filename='xontrib-macro-lib-docker-run-in-xonsh-docker')
+        cmd = f"docker run -it --rm {self.docker_image} {self.docker_executor} -c {repr(self.macro_block)}"
+        __xonsh__.builtins.execx(cmd, 'exec', __xonsh__.ctx, filename='xontrib-macro-lib-docker-run-in-docker')
 
+    
 
+class RunInXonshDocker(RunInDocker):
+    """Run xonsh codeblock in the xonsh docker container."""
+
+    def __init__(self, image='xonsh/xonsh:slim', executor='/usr/local/bin/xonsh'):
+        super().__init__(image, executor)
