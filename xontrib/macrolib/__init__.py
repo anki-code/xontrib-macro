5d4a9964cd3d5052a5cf1052021f7bcb6d0481e6
Andy Kipp <anki-code@users.noreply.github.com>
2023-03-19 20:21:47 +0600

    Update __init__.py

--- xontrib/macrolib/__init__.py
+++ xontrib/macrolib/__init__.py
@@ -1,8 +1,8 @@
 """Library of the useful macro for the xonsh shell."""
 
 def _load_xontrib_(*args, **kwargs) -> dict:
-    import xontrib.macrolib as macrolib
-    import xontrib.macrolib.data
-    import xontrib.macrolib.docker
-    import xontrib.macrolib.run
-    return {'macrolib': macrolib}
+    import xontrib.macro as macro
+    import xontrib.macro.data
+    import xontrib.macro.docker
+    import xontrib.macro.run
+    return {'macro': macro}
